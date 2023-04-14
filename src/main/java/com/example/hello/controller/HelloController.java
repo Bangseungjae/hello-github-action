6cb14535c4c2e11de0327cff52f02a04a0e9be31
Bangseungjae <tmdwo5825@naver.com>
2023-04-14 21:43:22 +0900

    first commit

---
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.hello.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.of(Optional.of("Hello, Github Actions"));
+    }
+}
